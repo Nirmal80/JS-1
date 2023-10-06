95c9fb47dc20f7fde1aab68c8b5998e85812bcec
Nirmal Mondal <147086330+Nirmal80@users.noreply.github.com>
2023-10-06 11:11:23 +0000

    class 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "mandalnirmal089@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+let accountState;
+
+// accountId = 2 // not allo
+
+// console.log(accountId);
+
+accountEmail = "mandalnirmal.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+// console.log(accuntId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+Prefer not use var
+because of issuse in block scope and functional scope
+*/
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all js code as newer version
+
+// alert (6 + 6 ) //we  are using nodejs,not browser
+
+console.log(3 + 3); console.log("nirmal")
