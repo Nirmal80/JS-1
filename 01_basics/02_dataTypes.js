f0acdc7b2ea564d1a8dbf801a2e59d755235fcf8
Nirmal Mondal <147086330+Nirmal80@users.noreply.github.com>
2023-10-08 08:00:44 +0000

    data-types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,3 +3,22 @@
 // alert (6 + 6 ) //we  are using nodejs,not browser
 
 console.log(3 + 3); console.log("nirmal")
+
+let name = "nirmal"
+let age = 18
+let isloggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// underfined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
